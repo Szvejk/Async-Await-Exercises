bc899b7a674f14bf639a84f5f236486a07df64e4
Sara Szwejkowska <saraszw@op.pl>
2022-12-01 13:16:46 +0100

    firt exercise

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+// ZADANIE 1
+// async function getData("https://restcountries.com/v3.1/all") {
+// 	const countriesAPI = await fetch("https://restcountries.com/v3.1/all")
+// const data = await response.json()
+// return data
+// }
+// async function getMoreData(){
+// 	const countries = await getData("https://restcountries.com/v3.1/all")
+// 	const cats = await getData("https://api.thecatapi.com/v1/breeds")
+// 	return data;
+// }
+// const variables = arr.map(function(data){
+// 	data.forEach(element => element = country||capital||official languages||area|| population)
+// 			console.log(element)
+		
+// 	});
+
+// 	getMoreData().then(data =>{
+// 	console.log(data)
+// })
+
+// B
+
+// async function getData("https://api.thecatapi.com/v1/breeds") {
+// 	const countriesAPI = await fetch("https://api.thecatapi.com/v1/breeds")
+// const data = await response.json()
+// return data
+// }
+// async function getMoreData(){
+// 	const cats = await getData("https://api.thecatapi.com/v1/breeds")
+// 	return cats["name"]
+// }
+// getMoreData().then(data =>{
+// 	console.log(data)
+// })
+
+
+
+
+
+
+// C
+// async function getData("https://restcountries.com/v3.1/all"){
+	
+// / 	const countriesAPI = await fetch("https://restcountries.com/v3.1/all")
+// // const data = await response.json()
+// // return data }
+// // async function getMoreData(){
+// // 	const countries = await getData("https://restcountries.com/v3.1/all")
+// / 	return data
+// // }
+// const variables = arr.map(function(data){
+// // let area  = data.sort((a,b) => b-a). slice (0,1)
+// // console.log(area)
+
+// // let population  = data.sort((a,b) => b-a). slice (0,1)
+// // console.log(population)
+// })
+// getMoreData().then(data=>{
+// 	console.log(data)
+// })
+// f(getData).catch(alert);
